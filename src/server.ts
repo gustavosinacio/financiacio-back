89f0dea46eff3ea716e3c45ff04a5cdb8487c239
gustavosinacio <gsinacio94@gmail.com>
2022-12-02 20:56:49 -0300

    adds ts config for import mapping

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { NextFunction, Request, Response } from "express";
-import swaggerUi from "swagger-ui-express";
+import express, { NextFunction, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
-import { AppDataSource } from "./database";
-import { routes } from "./routes";
-import swaggerSetup from "./swagger.json";
+import { AppDataSource } from 'src/database';
+import { routes } from './routes';
+import swaggerSetup from './swagger.json';
 
 const app = express();
 const port = 3333;
@@ -12,14 +12,14 @@ AppDataSource.initialize();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log("*---* running");
-  res.json({ message: "financiacio api" });
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSetup));
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log('*---* running');
+  res.json({ message: 'financiacio api' });
 });
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log("*---*", req.method, req.url);
+  console.log('*---*', req.method, req.url);
   next();
 });
 
